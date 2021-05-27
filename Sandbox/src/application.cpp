54fe01dc70a7b6a3070d0024041118ba51eb6c38
xero-lib <jonathankurtz@protonmail.com>
2021-05-27 04:01:52 -0500

    Just some small testing stuff

--- Sandbox/src/application.cpp
+++ Sandbox/src/application.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 
 namespace XME
 {
@@ -10,4 +10,17 @@ int main()
 {
     XME::Print();
     printf("\n");
+
+    //print all chars
+    for(int i = 33; i < 127; i++) {
+        printf("%c", (char)i);
+    }
+
+    std::cout << std::endl;
+    
+    //print sizes
+    std::cout << "unsigned long long:\t" << sizeof(unsigned long long) << std::endl;
+    std::cout << "size_t:\t"             << sizeof(size_t)             << std::endl;
+    std::cout << "int:\t"                << sizeof(int)                << std::endl;
+    std::cout << "char:\t"               << sizeof(char)               << std::endl;
 }
